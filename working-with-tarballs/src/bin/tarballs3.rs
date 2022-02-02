30d80650f09986cbef8062c7548f483b0201dfad
shota-tsuji <shotatuzi@gmail.com>
2022-02-03 05:38:00 +0900

    feat:  Working with Tarballs

---
+++ working-with-tarballs/src/bin/tarballs3.rs
@@ -0,0 +1,32 @@
+use error_chain::error_chain;
+use std::fs::File;
+use std::path::PathBuf;
+use flate2::read::GzDecoder;
+use tar::Archive;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        StripPrefixError(::std::path::StripPrefixError);
+    }
+}
+
+fn main() -> Result<()> {
+    let file = File::open("archive.tar.gz")?;
+    let mut  archive = Archive::new(GzDecoder::new(file));
+    let prefix = "backup/logs";
+
+    println!("Extracted the following files:");
+    archive
+        .entries()?
+        .filter_map(|e| e.ok())
+        .map(|mut entry| -> Result<PathBuf> {
+            let path = entry.path()?.strip_prefix(prefix)?.to_owned();
+            entry.unpack(&path)?;
+            Ok(path)
+        })
+        .filter_map(|e| e.ok())
+        .for_each(|x| println!("> {}", x.display()));
+
+    Ok(())
+}
